8faec80f112aad8598a0e2fa23e3beb62d193c16
Carlos Enrique Rodriguez <lw4dbf@gmail.com>
2015-03-13 15:02:51 -0300

    Lista la persistencia de datos y ya casi la pantalla de ingreso

--- src/dmtrabajos.pas
+++ src/dmtrabajos.pas
@@ -14,6 +14,48 @@ type
   { TDM_Trabajos }
 
   TDM_Trabajos = class(TDataModule)
+    Equipos: TRxMemoryData;
+    ClientesbVisible: TLongintField;
+    Clientesdocumento: TStringField;
+    ClientesDomicilio: TStringField;
+    Clientesid: TLongintField;
+    Clientesmail: TStringField;
+    ClientesNombre: TStringField;
+    ClientesNotras: TStringField;
+    ClientesOrganizacion: TStringField;
+    ClientesTelefono: TStringField;
+    Marcas: TRxMemoryData;
+    EquiposbVisible: TLongintField;
+    EquiposbVisible1: TLongintField;
+    Equiposid: TLongintField;
+    Equiposid1: TLongintField;
+    EquiposMarca: TStringField;
+    EquiposMarca1: TStringField;
+    Equiposmarca_id: TLongintField;
+    Equiposmarca_id1: TLongintField;
+    Equiposmodelo: TStringField;
+    Equiposmodelo1: TStringField;
+    Equiposnotas: TStringField;
+    Equiposnotas1: TStringField;
+    EquiposnSerie: TStringField;
+    EquiposnSerie1: TStringField;
+    qSELTrabajobEgreso: TLargeintField;
+    qSELTrabajobVisible: TLargeintField;
+    qSELTrabajocliente_id: TLargeintField;
+    qSELTrabajoDetalle: TStringField;
+    qSELTrabajoDiagnostico: TStringField;
+    qSELTrabajoequipo_id: TLargeintField;
+    qSELTrabajofCompromiso: TDateField;
+    qSELTrabajofEgreso: TDateField;
+    qSELTrabajofIngreso: TDateField;
+    qSELTrabajoid: TLargeintField;
+    Trabajos: TRxMemoryData;
+    qDELEquipos: TZQuery;
+    qDELMarcas: TZQuery;
+    qDELMarcas1: TZQuery;
+    qINSEquipos: TZQuery;
+    qINSMarcas: TZQuery;
+    qINSTrabajos: TZQuery;
     qSELClientes: TZQuery;
     qPendientesCliente: TStringField;
     qPendientesCompromiso: TDateField;
@@ -34,15 +76,100 @@ type
     Clientes: TRxMemoryData;
     qPendientes: TZQuery;
     qINSClientes: TZQuery;
+    qSELClientesbVisible10: TLargeintField;
+    qSELClientesbVisible12: TLargeintField;
+    qSELClientesbVisible13: TLargeintField;
+    qSELClientesbVisible8: TLargeintField;
+    qSELClientesbVisible9: TLargeintField;
+    qSELClientesdocumento10: TStringField;
+    qSELClientesdocumento12: TStringField;
+    qSELClientesdocumento13: TStringField;
+    qSELClientesdocumento8: TStringField;
+    qSELClientesdocumento9: TStringField;
+    qSELClientesDomicilio10: TStringField;
+    qSELClientesDomicilio12: TStringField;
+    qSELClientesDomicilio13: TStringField;
+    qSELClientesDomicilio8: TStringField;
+    qSELClientesDomicilio9: TStringField;
+    qSELClientesemail10: TStringField;
+    qSELClientesemail12: TStringField;
+    qSELClientesemail13: TStringField;
+    qSELClientesemail8: TStringField;
+    qSELClientesemail9: TStringField;
+    qSELClientesid10: TLargeintField;
+    qSELClientesid12: TLargeintField;
+    qSELClientesid13: TLargeintField;
+    qSELClientesid8: TLargeintField;
+    qSELClientesid9: TLargeintField;
+    qSELClientesNombre10: TStringField;
+    qSELClientesNombre12: TStringField;
+    qSELClientesNombre13: TStringField;
+    qSELClientesNombre8: TStringField;
+    qSELClientesNombre9: TStringField;
+    qSELClientesnotas10: TStringField;
+    qSELClientesnotas12: TStringField;
+    qSELClientesnotas13: TStringField;
+    qSELClientesnotas8: TStringField;
+    qSELClientesnotas9: TStringField;
+    qSELClientesOrganizacion10: TStringField;
+    qSELClientesOrganizacion12: TStringField;
+    qSELClientesOrganizacion13: TStringField;
+    qSELClientesOrganizacion8: TStringField;
+    qSELClientesOrganizacion9: TStringField;
+    qSELClientesTelefono10: TStringField;
+    qSELClientesTelefono12: TStringField;
+    qSELClientesTelefono13: TStringField;
+    qSELClientesTelefono8: TStringField;
+    qSELClientesTelefono9: TStringField;
+    qSELEquipos: TZQuery;
     qSELClientesbVisible3: TLargeintField;
+    qSELClientesbVisible5: TLargeintField;
+    qSELClientesbVisible6: TLargeintField;
+    qSELClientesbVisible7: TLargeintField;
     qSELClientesdocumento3: TStringField;
+    qSELClientesdocumento5: TStringField;
+    qSELClientesdocumento6: TStringField;
+    qSELClientesdocumento7: TStringField;
     qSELClientesDomicilio3: TStringField;
+    qSELClientesDomicilio5: TStringField;
+    qSELClientesDomicilio6: TStringField;
+    qSELClientesDomicilio7: TStringField;
     qSELClientesemail3: TStringField;
+    qSELClientesemail5: TStringField;
+    qSELClientesemail6: TStringField;
+    qSELClientesemail7: TStringField;
     qSELClientesid3: TLargeintField;
+    qSELClientesid5: TLargeintField;
+    qSELClientesid6: TLargeintField;
+    qSELClientesid7: TLargeintField;
     qSELClientesNombre3: TStringField;
+    qSELClientesNombre5: TStringField;
+    qSELClientesNombre6: TStringField;
+    qSELClientesNombre7: TStringField;
     qSELClientesnotas3: TStringField;
+    qSELClientesnotas5: TStringField;
+    qSELClientesnotas6: TStringField;
+    qSELClientesnotas7: TStringField;
     qSELClientesOrganizacion3: TStringField;
+    qSELClientesOrganizacion5: TStringField;
+    qSELClientesOrganizacion6: TStringField;
+    qSELClientesOrganizacion7: TStringField;
     qSELClientesTelefono3: TStringField;
+    qSELClientesTelefono5: TStringField;
+    qSELClientesTelefono6: TStringField;
+    qSELClientesTelefono7: TStringField;
+    qSELMarcas: TZQuery;
+    qSELEquiposbVisible: TLargeintField;
+    qSELEquiposid: TLargeintField;
+    qSELEquiposMarca: TStringField;
+    qSELEquiposmarca_id: TLargeintField;
+    qSELEquiposmodelo: TStringField;
+    qSELEquiposnotas: TStringField;
+    qSELEquiposnSerie: TStringField;
+    qSELTrabajo: TZQuery;
+    qSELMarcasbVisible: TLargeintField;
+    qSELMarcasid: TLargeintField;
+    qSELMarcasMarca: TStringField;
     qUPDClientes: TZQuery;
     qSELClientesbVisible: TLargeintField;
     qSELClientesbVisible1: TLargeintField;
@@ -72,12 +199,31 @@ type
     qSELClientesTelefono1: TStringField;
     qSELClientesTelefono2: TStringField;
     qDELClientes: TZQuery;
+    qUPDEquipos: TZQuery;
+    qUPDMarcas: TZQuery;
+    qUPDTrabajos: TZQuery;
+    TrabajosbEgreso: TLongintField;
+    TrabajosbVisible: TLongintField;
+    Trabajoscliente_id: TLongintField;
+    TrabajosDetalle: TStringField;
+    TrabajosDiagnostico: TStringField;
+    Trabajosequipo_id: TLongintField;
+    TrabajosfCompromiso: TDateTimeField;
+    TrabajosfEgreso: TDateTimeField;
+    TrabajosfIngreso: TDateTimeField;
+    Trabajosid: TLongintField;
     procedure ClientesAfterInsert(DataSet: TDataSet);
+    procedure EquiposAfterInsert(DataSet: TDataSet);
+    procedure MarcasAfterInsert(DataSet: TDataSet);
+    procedure TrabajosAfterInsert(DataSet: TDataSet);
 
   private
-    { private declarations }
+    procedure AbrirTablas;
   public
     procedure EquiposPendientes;
+    procedure TrabajoInsertar;
+    procedure TrabajoEditar (id: Integer);
+    procedure TrabajoGrabar;
   end;
 
 var
@@ -96,6 +242,41 @@ begin
   end;
 end;
 
+procedure TDM_Trabajos.EquiposAfterInsert(DataSet: TDataSet);
+begin
+  with DataSet do
+  begin
+    FieldByName('marca_id').asInteger:= 0;
+    FieldByName('bVisible').asInteger:= 1;
+  end;
+end;
+
+procedure TDM_Trabajos.MarcasAfterInsert(DataSet: TDataSet);
+begin
+   with DataSet do
+  begin
+    FieldByName('bVisible').asInteger:= 1;
+  end;
+end;
+
+procedure TDM_Trabajos.TrabajosAfterInsert(DataSet: TDataSet);
+begin
+  with DataSet do
+  begin
+    FieldByName('fIngreso').AsDateTime:= Now;
+    FieldByName('bEgreso').asInteger:= 0;
+    FieldByName('bVisible').asInteger:= 1;
+  end;
+end;
+
+procedure TDM_Trabajos.AbrirTablas;
+begin
+  DM_General.ReiniciarTabla(Equipos);
+  DM_General.ReiniciarTabla(Clientes);
+  DM_General.ReiniciarTabla(Marcas);
+  DM_General.ReiniciarTabla(Trabajos);
+end;
+
 procedure TDM_Trabajos.EquiposPendientes;
 begin
   with qPendientes do
@@ -103,6 +284,26 @@ begin
     if active then close;
     Open;
   end;
+end;
+
+procedure TDM_Trabajos.TrabajoInsertar;
+begin
+  AbrirTablas;
+
+end;
+
+procedure TDM_Trabajos.TrabajoEditar(id: Integer);
+begin
+  AbrirTablas;
+
+end;
+
+procedure TDM_Trabajos.TrabajoGrabar;
+begin
+  DM_General.GrabarDatos(qSELClientes, qINSClientes, qUPDClientes, Clientes, 'id');
+  DM_General.GrabarDatos(qSELEquipos, qINSEquipos, qUPDEquipos, Equipos, 'id');
+  DM_General.GrabarDatos(qSELMarcas, qINSMarcas, qUPDMarcas, Marcas, 'id');
+  DM_General.GrabarDatos(qSELTrabajo, qINSTrabajos, qUPDTrabajos, Trabajos, 'id');
 end;
 
 end.
--- src/frm_trabajoae.pas
+++ src/frm_trabajoae.pas
@@ -5,36 +5,68 @@ unit frm_TrabajoAE;
 interface
 
 uses
-  Classes, SysUtils, db, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ExtCtrls, DbCtrls;
+  Classes, SysUtils, db, FileUtil, dbdateedit, Forms, Controls, Graphics,
+  Dialogs, StdCtrls, ExtCtrls, DbCtrls, Buttons, ComCtrls, dmgeneral;
 
 type
 
   { TfrmTrabajoAE }
 
   TfrmTrabajoAE = class(TForm)
+    btnSalir: TBitBtn;
+    BitBtn2: TBitBtn;
+    btnCancelar: TBitBtn;
+    DBDateEdit1: TDBDateEdit;
+    DBDateEdit2: TDBDateEdit;
+    DBDateEdit3: TDBDateEdit;
     DBEdit1: TDBEdit;
     DBEdit2: TDBEdit;
-    DBEdit3: TDBEdit;
-    DBEdit4: TDBEdit;
-    DBEdit5: TDBEdit;
+    DBLookupComboBox1: TDBLookupComboBox;
+    DBMemo2: TDBMemo;
+    DBMemo3: TDBMemo;
+    DBMemo4: TDBMemo;
+    dbNombre: TDBEdit;
+    dbOrganismo: TDBEdit;
+    dbDocumento: TDBEdit;
+    dbTelefono: TDBEdit;
+    dbEmail: TDBEdit;
     DBEdit6: TDBEdit;
     DBMemo1: TDBMemo;
     ds_Clientes: TDataSource;
+    ds_Equipos: TDataSource;
+    ds_Marcas: TDataSource;
+    ds_Trabajos: TDataSource;
     gbCliente: TGroupBox;
     GroupBox2: TGroupBox;
     GroupBox3: TGroupBox;
     Label1: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    Label16: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Label4: TLabel;
     Label5: TLabel;
     Label6: TLabel;
     Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    PageControl1: TPageControl;
     Panel1: TPanel;
+    SpeedButton1: TSpeedButton;
+    tabIngreso: TTabSheet;
+    tabEgreso: TTabSheet;
+    procedure btnCancelarClick(Sender: TObject);
+    procedure btnSalirClick(Sender: TObject);
+    procedure dbNombreKeyPress(Sender: TObject; var Key: char);
+    procedure FormShow(Sender: TObject);
   private
     _trabajo_id: integer;
-    { private declarations }
+    procedure Buscar(dato, componente: string);
   public
     property idTrabajo: integer read _trabajo_id write _trabajo_id;
   end;
@@ -43,8 +75,58 @@ var
   frmTrabajoAE: TfrmTrabajoAE;
 
 implementation
-
 {$R *.lfm}
+uses
+  dmtrabajos;
+
+{ TfrmTrabajoAE }
+
+procedure TfrmTrabajoAE.dbNombreKeyPress(Sender: TObject; var Key: char);
+var
+  dato, componente: string;
+begin
+  if Key = #13 then
+  begin
+    dato:=(Sender as TDBEdit).Text;
+    componente :=(Sender as TDBEdit).Name;
+    Buscar (dato, componente);
+  end;
+end;
+
+procedure TfrmTrabajoAE.btnSalirClick(Sender: TObject);
+begin
+  DM_Trabajos.TrabajoGrabar;
+  ModalResult:= mrOK;
+end;
+
+procedure TfrmTrabajoAE.btnCancelarClick(Sender: TObject);
+begin
+  if (MessageDlg ('ATENCION', 'Cancela la carga y sale de la pantalla?', mtConfirmation, [mbYes, mbNo],0 ) = mrYes) then
+    ModalResult:= mrCancel;
+end;
+
+procedure TfrmTrabajoAE.FormShow(Sender: TObject);
+begin
+  if idTrabajo = ID_NULO then
+  begin
+    DM_Trabajos.TrabajoInsertar;
+  end
+  else
+  begin
+    DM_Trabajos.TrabajoEditar (idTrabajo);
+  end;
+end;
+
+procedure TfrmTrabajoAE.Buscar(dato, componente: string);
+var
+  consulta: string;
+begin
+  dato := TRIM(dato);
+  if dato <> EmptyStr then
+  begin
+
+  end;
+end;
 
 end.
 
